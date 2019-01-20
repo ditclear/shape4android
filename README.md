cd0b4b40b0c133cf049a50cdd5c556501f65ff1f
ditclear <ditclear@qq.com>
2019-01-20 15:24:04 +0800

    update readme

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # shape4android
 > 使用Vue快速生成Android的背景xml
 >
-> 免去手动重复创建多个android xml文件的烦恼
+> 免去手动重复创建多个android xml文件的烦恼，可自定义名称
+
+#### Feature
+
+- 快速生成
+- 可视化操作
+- 可自定义文件名称
 
 
 
@@ -13,11 +19,11 @@
 
 #### 默认命名规则
 
-| shape    | shape_type_color_roundTL_roundTR_roundBL_roundBR_borderWidth_borderColor.xml |
-| -------- | ------------------------------------------------------------ |
-| selector | selector_shape_n_color_p_pressedColor_u_unableColor.xml      |
+shape： `shape_type_color_roundTL_roundTR_roundBL_roundBR_borderWidth_borderColor.xml`  
 
-> 有更好的命名规则，欢迎提issue
+selector：`selector_shape_n_color_p_pressedColor_u_unableColor.xml`
+
+> PS:如果自定义名称，那么selector中的shape默认会跟上type名，比如xx_norm.xml/xx_pressed.xml/xx_unable.xml
 
 #### License
 
--- src/components/Shape.vue
+++ src/components/Shape.vue
@@ -9,7 +9,7 @@
     <el-container>
       <el-aside width="600px">
         <el-col :span="16">
-          <el-radio-group v-model="tabPosition" style="margin-top: 30px;">
+          <el-radio-group v-model="tabPosition" style="margin-top: 50px;">
             <el-radio-button label="unable">unable</el-radio-button>
             <el-radio-button label="normal">normal</el-radio-button>
             <el-radio-button label="pressed">pressed</el-radio-button>
@@ -18,7 +18,7 @@
         <div
           class="result"
           v-bind:style="{'background': realColor ,'border-radius':realRound+'px',
-          'border': result.border_width + 'px solid '+result.border_color ,
+          'border': normal.border_width + 'px solid '+normal.border_color ,
           'border-top-left-radius':realRoundTL+'px','border-top-right-radius':realRoundTR+'px',
           'border-bottom-left-radius':realRoundBL+'px','border-bottom-right-radius':realRoundBR+'px'}"
         ></div>
@@ -38,7 +38,7 @@
                 <el-form-item label>
                   <el-col :span="3">形状</el-col>
                   <el-col :span="5">
-                    <el-select v-model="result.shape" placeholder="请选择">
+                    <el-select v-model="normal.shape" placeholder="请选择">
                       <el-option
                         v-for="item in options"
                         :key="item.value"
@@ -49,13 +49,13 @@
                   </el-col>
                   <el-col :span="3">颜色：</el-col>
                   <el-col :span="1">
-                    <el-color-picker v-model="result.color"></el-color-picker>
+                    <el-color-picker v-model="normal.color"></el-color-picker>
                   </el-col>
                 </el-form-item>
                 <el-form-item>
                   <el-col :span="3">角度</el-col>
                   <el-col :span="5">
-                    <el-input v-model="result.round" :disabled="!simpleRound">
+                    <el-input v-model="normal.round" :disabled="!simpleRound">
                       <template slot="append">dp</template>
                     </el-input>
                   </el-col>
@@ -73,26 +73,26 @@
                         <div v-show="!simpleRound">
                           <el-col :span="3">左上角</el-col>
                           <el-col :span="5">
-                            <el-input v-model="result.round_tl">
+                            <el-input v-model="normal.round_tl">
                               <template slot="append">dp</template>
                             </el-input>
                           </el-col>
                           <el-col :span="3">左下角</el-col>
                           <el-col :span="5">
-                            <el-input v-model="result.round_tr">
+                            <el-input v-model="normal.round_tr">
                               <template slot="append">dp</template>
                             </el-input>
                           </el-col>
                           <el-col>
                             <el-col :span="3">右上角</el-col>
                             <el-col :span="5">
-                              <el-input v-model="result.round_bl">
+                              <el-input v-model="normal.round_bl">
                                 <template slot="append">dp</template>
                               </el-input>
                             </el-col>
                             <el-col :span="3">右下角</el-col>
                             <el-col :span="5">
-                              <el-input v-model="result.round_br">
+                              <el-input v-model="normal.round_br">
                                 <template slot="append">dp</template>
                               </el-input>
                             </el-col>
@@ -105,13 +105,13 @@
                 <el-form-item label>
                   <el-col :span="3">边框</el-col>
                   <el-col :span="5">
-                    <el-input v-model="result.border_width">
+                    <el-input v-model="normal.border_width">
                       <template slot="append">dp</template>
                     </el-input>
                   </el-col>
                   <el-col class="line" :span="3">边框颜色：</el-col>
                   <el-col :span="1">
-                    <el-color-picker v-model="result.border_color"></el-color-picker>
+                    <el-color-picker v-model="normal.border_color"></el-color-picker>
                   </el-col>
                 </el-form-item>
                 <el-form-item>
@@ -149,19 +149,52 @@
                   </el-col>
                 </el-form-item>
                 <el-form-item>
-                  <el-col span="3">
-                    <el-button type="primary" icon="el-icon-download" @click="saveAsXml">下载</el-button>
+                  <el-col :span="4">
+                    <el-checkbox v-model="needOverride">自定义文件名</el-checkbox>
+                  </el-col>
+                  <el-form-item>
+                    <el-col span="3">
+                      <el-button type="text" @click="dialogVisible = true">➡️默认命名规则</el-button>
+                    </el-col>
+                    <el-dialog
+                      title="默认命名规则"
+                      :visible.sync="dialogVisible"
+                      width="50%"
+                      :before-close="handleClose"
+                      center="false"
+                    >
+                      <el-form-item label="shape:" label-width="100px">
+                        <el-col
+                          span="5"
+                        >shape_type_color_roundTL_roundTR_roundBL_roundBR_borderWidth_borderColor.xml</el-col>
+                      </el-form-item>
+
+                      <el-form-item label="selector:" label-width="100px">
+                        <el-col span="5" top>selector_shape_n_color_p_pressedColor_u_unableColor.xml</el-col>
+                      </el-form-item>
+                      <div>注意：</div>
+                      <span>
+                        如果自定义名称，那么selector中的shape默认会跟上type名，比如
+                        xx_norm.xml/xx_pressed.xml/xx_unable.xml
+                      </span>
+                    </el-dialog>
+                  </el-form-item>
+                </el-form-item>
+                <el-form-item>
+                  <el-col>
+                    <el-collapse-transition>
+                      <div v-show="needOverride">
+                        <el-col :span="10" style="margin-left:20px">
+                          <el-input v-model="shapeName" placeholder="请输入文件名"></el-input>
+                        </el-col>
+                      </div>
+                    </el-collapse-transition>
                   </el-col>
                 </el-form-item>
-                <el-form-item label="命名规则"></el-form-item>
-                <el-form-item label="shape:" label-width="100px">
-                  <el-col
-                    span="5"
-                  >shape_type_color_roundTL_roundTR_roundBL_roundBR_borderWidth_borderColor.xml</el-col>
-                </el-form-item>
-
-                <el-form-item label="selector:" label-width="100px">
-                  <el-col span="5" top>selector_shape_n_color_p_pressedColor_u_unableColor.xml</el-col>
+                <el-form-item>
+                  <el-col span="5">
+                    <el-button type="primary" icon="el-icon-download" @click="saveAsXml">下载</el-button>
+                  </el-col>
                 </el-form-item>
               </el-form>
             </div>
@@ -179,12 +212,16 @@ export default {
   name: "Shape",
   data() {
     return {
+      dialogVisible: false,
       labelPosition: "left",
       tabPosition: "normal",
-      needPresse: false,
-      needUnable: false,
-      simpleRound: true,
+      shapeName: "", //自定义的文件名
+      needOverride: false, //需要自定义文件名称
+      needPresse: false, //需要pressed文件
+      needUnable: false, //需要unable文件
+      simpleRound: true, //统一使用相同的圆角
       options: [
+        //形状，oval or rectangle
         {
           value: "rectangle",
           label: "rectangle"
@@ -194,8 +231,7 @@ export default {
           label: "oval"
         }
       ],
-      shapeXml: "",
-      result: {
+      normal: {
         shape: "rectangle",
         round: "0",
         round_tl: "0",
@@ -216,55 +252,50 @@ export default {
       }
     };
   },
-  mounted() {
-    this.$refs.range.oninput = ({ target: { value } }) => {
-      this.width = `${60 + value * 0.4}%`;
-    };
-  },
   computed: {
     // 计算属性的 getter
     realRound: function() {
       // `this` 指向 vm 实例
-      if (this.result.shape == "oval") {
+      if (this.normal.shape == "oval") {
         return 100;
       }
-      return this.result.round;
+      return this.normal.round;
     },
     realRoundTL() {
-      if (this.result.shape == "oval") {
+      if (this.normal.shape == "oval") {
         return 100;
       }
       if (this.simpleRound) {
-        return this.result.round;
+        return this.normal.round;
       }
-      return this.result.round_tl;
+      return this.normal.round_tl;
     },
     realRoundTR() {
-      if (this.result.shape == "oval") {
+      if (this.normal.shape == "oval") {
         return 100;
       }
       if (this.simpleRound) {
-        return this.result.round;
+        return this.normal.round;
       }
-      return this.result.round_tr;
+      return this.normal.round_tr;
     },
     realRoundBL() {
-      if (this.result.shape == "oval") {
+      if (this.normal.shape == "oval") {
         return 100;
       }
       if (this.simpleRound) {
-        return this.result.round;
+        return this.normal.round;
       }
-      return this.result.round_bl;
+      return this.normal.round_bl;
     },
     realRoundBR() {
-      if (this.result.shape == "oval") {
+      if (this.normal.shape == "oval") {
         return 100;
       }
       if (this.simpleRound) {
-        return this.result.round;
+        return this.normal.round;
       }
-      return this.result.round_br;
+      return this.normal.round_br;
     },
     realColor: function() {
       if (this.tabPosition == "pressed" && this.needPresse) {
@@ -272,11 +303,17 @@ export default {
       } else if (this.tabPosition == "unable" && this.needUnable) {
         return this.unable.color;
       } else {
-        return this.result.color;
+        return this.normal.color;
       }
     }
   },
   methods: {
+    /**
+     * 组装shape的xml
+     * @augments type normal/pressed/unable
+     * @augments color 颜色
+     * @returns fileName,xml
+     */
     provideXmlTemplate(type, color) {
       var xml =
         "<?xml version=" +
@@ -287,17 +324,17 @@ export default {
         "<shape xmlns:android=" +
         '"http://schemas.android.com/apk/res/android"\n' +
         "android:shape=" +
-        this.wrapWithQuote(this.result.shape) +
+        this.wrapWithQuote(this.normal.shape) +
         ">\n" +
         "<solid android:color=" +
         this.wrapWithQuote(color) +
         "/>\n" +
         "<stroke android:color=" +
-        this.wrapWithQuote(this.result.border_color) +
+        this.wrapWithQuote(this.normal.border_color) +
         " android:width=" +
-        this.wrapWithQuote(this.result.border_width + "dp") +
+        this.wrapWithQuote(this.normal.border_width + "dp") +
         "/>\n";
-      if (this.result.shape != "oval") {
+      if (this.normal.shape != "oval") {
         xml +=
           "<corners " +
           "android:topLeftRadius=" +
@@ -316,32 +353,40 @@ export default {
       }
 
       xml += "</shape>";
-
-      var fileName =
-        this.result.shape +
-        "_" +
-        type +
-        "_" +
-        color.slice(1, 7) +
-        "_" +
-        this.realRoundTL +
-        "_" +
-        this.realRoundTR +
-        "_" +
-        this.realRoundBL +
-        "_" +
-        this.realRoundBR +
-        "_" +
-        this.result.border_width +
-        "_" +
-        this.result.border_color.slice(1, 7);
+      var fileName = "";
+      if (!this.needOverride) {
+        fileName =
+          this.normal.shape +
+          "_" +
+          type +
+          "_" +
+          color.slice(1, 7) +
+          "_" +
+          this.realRoundTL +
+          "_" +
+          this.realRoundTR +
+          "_" +
+          this.realRoundBL +
+          "_" +
+          this.realRoundBR +
+          "_" +
+          this.normal.border_width +
+          "_" +
+          this.normal.border_color.slice(1, 7);
+      } else {
+        fileName = this.shapeName + "_" + type;
+      }
 
       return { fileName: fileName, xml: xml };
     },
-    provideSelecter() {
+    /**
+     * 组装selector
+     * */
+
+    provideSelector() {
       var JSZip = require("jszip");
       var zip = new JSZip();
-      var normalFile = this.provideXmlTemplate("normal", this.result.color);
+      var normalFile = this.provideXmlTemplate("normal", this.normal.color);
       var normalFileName = normalFile.fileName;
       var normalXml = normalFile.xml;
       zip.file(normalFileName + ".xml", normalXml);
@@ -360,7 +405,6 @@ export default {
         var unableXml = unableFile.xml;
         zip.file(unableFileName + ".xml", unableXml);
       }
-
       var selectorXml =
         "<?xml version=" +
         this.wrapWithQuote("1.0") +
@@ -379,7 +423,7 @@ export default {
         "/>\n";
 
       var fileName =
-        "selector_" + this.result.shape + "_n_" + this.result.color.slice(1, 7);
+        "selector_" + this.normal.shape + "_n_" + this.normal.color.slice(1, 7);
 
       if (this.needPresse) {
         selectorXml +=
@@ -405,39 +449,56 @@ export default {
       }
 
       selectorXml += "</selector>";
-
+      if (this.needOverride) {
+        fileName = this.shapeName;
+      }
       zip.file(fileName + ".xml", selectorXml);
       zip.generateAsync({ type: "blob" }).then(function(content) {
         // see FileSaver.js
         saveAs(content, fileName + ".zip");
       });
     },
-    generateXml() {
+    /**
+     * 生成xml
+     */
+    saveAsXml() {
+      this.shapeName = this.shapeName.trim();
+
+      if (this.needOverride && this.shapeName == "") {
+        this.warning("请输入文件名称");
+        return;
+      }
       if (!this.needPresse && !this.needUnable) {
         var { fileName, xml } = this.provideXmlTemplate(
           "normal",
-          this.result.color
+          this.normal.color
         );
+        if (this.needOverride) {
+          fileName = this.shapeName;
+        }
         var blob = new Blob([xml], {
           type: "text/plain;charset=utf-8"
         });
         saveAs(blob, fileName + ".xml");
       } else {
-        this.provideSelecter();
+        this.provideSelector();
       }
-    },
-    saveAsXml() {
-      this.generateXml();
-      this.open3("下载成功");
+      this.toast("下载成功");
     },
     wrapWithQuote(param) {
       return '"' + param + '"';
     },
-    open3(message) {
+    toast(message) {
       this.$notify({
         title: "成功",
         message: message,
         type: "success"
+      });
+    },
+    warning(message) {
+      this.$message({
+        message: message,
+        type: "warning"
       });
     }
   }
@@ -479,7 +540,7 @@ export default {
 
 .result {
   max-width: 200px;
-  margin-top: 100px;
+  margin-top: 125px;
   margin-left: 100px;
   width: 200px;
   height: 200px;
@@ -515,42 +576,45 @@ p {
   font-family: "Helvetica Neue";
 }
 .ribbon {
-    position: absolute;
-    top: 42px;
-    width: 200px;
-    padding: 1px 0;
-    background: #000;
-    font-family: "Helvetica Neue";
-    color: #eee;
-    -moz-box-shadow: 0 0 10px rgba(0,0,0,0.5);
-    -webkit-box-shadow: 0 0 10px rgba(0,0,0,0.5);
-    box-shadow: 0 0 10px rgba(0,0,0,0.5);
+  position: absolute;
+  top: 42px;
+  width: 200px;
+  padding: 1px 0;
+  background: #000;
+  font-family: "Helvetica Neue";
+  color: #eee;
+  -moz-box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  -webkit-box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
 }
 .ribbon.grey {
-    background: #888;
+  background: #888;
 }
 .ribbon.right {
-    right: -42px;
-    -moz-transform: rotate(45deg);
-    -webkit-transform: rotate(45deg);
-    -o-transform: rotate(45deg);
-    -ms-transform: rotate(45deg);
-    transform: rotate(45deg);
+  right: -42px;
+  -moz-transform: rotate(45deg);
+  -webkit-transform: rotate(45deg);
+  -o-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  transform: rotate(45deg);
 }
-.ribbon a, .ribbon a:visited, .ribbon a:active, .ribbon a:hover {
-    display: block;
-    padding: 1px 0;
-    height: 24px;
-    line-height: 24px;
-    color: inherit;
-    text-align: center;
-    text-decoration: none;
-    font-family: 'Cabin Condensed',sans-serif;
-    font-size: 16px;
-    font-weight: 500;
-    border: 1px solid rgba(255,255,255,0.3);
-    -moz-text-shadow: 0 0 10px rgba(0,0,0,0.31);
-    -webkit-text-shadow: 0 0 10px rgba(0,0,0,0.31);
-    text-shadow: 0 0 10px rgba(0,0,0,0.31);
+.ribbon a,
+.ribbon a:visited,
+.ribbon a:active,
+.ribbon a:hover {
+  display: block;
+  padding: 1px 0;
+  height: 24px;
+  line-height: 24px;
+  color: inherit;
+  text-align: center;
+  text-decoration: none;
+  font-family: "Cabin Condensed", sans-serif;
+  font-size: 16px;
+  font-weight: 500;
+  border: 1px solid rgba(255, 255, 255, 0.3);
+  -moz-text-shadow: 0 0 10px rgba(0, 0, 0, 0.31);
+  -webkit-text-shadow: 0 0 10px rgba(0, 0, 0, 0.31);
+  text-shadow: 0 0 10px rgba(0, 0, 0, 0.31);
 }
 </style>
